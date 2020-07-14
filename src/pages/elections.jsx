7505726134f5d3031051fa2d9a3418b432647bea
Kevin Shen <kevin_shen@hotmail.ca>
2020-07-13 21:35:05 -0400

    updated positions page

--- src/pages/elections.jsx
+++ src/pages/elections.jsx
@@ -37,64 +37,67 @@ const PositionsPage = () => {
     const { placeholderImage } = useStaticQuery(query);
     return (
         <Container tag='main' block='alg'>
-            <h1>CSEC 2020 Elections</h1>
+            <h1>CSEC 2020 Elections and Open Positions</h1>
             <div className='about__row'>
                 <div className='about__content'>
                     <p className='about__text'>
-                        Friday May 29th 6pm - 7pm on Zoom. Be sure to join our <a href='https://discord.gg/4tWwRM4' target='_blank' rel='noopener noreferrer'>Discord</a> for updates on our #announcements channel!
-                    </p>
-                    <p>
-                        <a href='https://zoom.us/j/9930709809?pwd=RnpDUVBsRFdqdG02RmM5YmpSUGFDZz09' target='_blank' rel='noopener noreferrer'>Zoom Meeting Url</a>
-                        <br />
-                    Meeting ID: 993 070 9809
-                        <br />
-                        Password: 0kW1vK
+                    Elections are over! We still have some open spots for the 2020 - 2021 school year below.
                     </p>
                     <p className='about__text'>
-                        General Elections for the Computer Science Enrichment Club are coming up! If you've enjoyed any of our events (World of Work, Mock Interviews, networking events, etc.) please join us in our annual elections! You can decide what the future of the CS community on campus looks like!
-                    </p>
-                    <p className='about__text'>
-                        Want to join the executive team? We'd be thrilled to have you! Simply fill out the form here and show up to our elections! You'll be expected to do a little introduction about yourself and your ideas. <b>For information about the positions, scroll down to see the descriptions.</b> Please fill this out by Friday May 29th at 4pm.
-                    <a className='alg__link' href='https://forms.gle/F9BaRpuxDkW3wack6' target='_blank' rel='noopener noreferrer'>
-                            Link to Application
-                        <FaExternalLinkAlt className='alg__link-icon' />
-                        </a>
+                        If you've enjoyed any of our events (World of Work, Mock Interviews, networking events, etc.) please join us in our annual elections! You can decide what the future of the CS community on campus looks like!
                     </p>
 
                 </div>
                 <Img className='alg__image' fluid={placeholderImage.childImageSharp.fluid} imgStyle={{ height: 'auto' }} />
             </div>
-            <h2 className='about__title'>Available Positions for the 2020 Elections</h2>
+            <h2 className='about__title'>Current Open 2020 - 2021 positions</h2>
+            <div>
+                <a href='https://docs.google.com/forms/d/1WwbdzGCXigH23A9hBp5YdJ1mIGQ60jOsamkvyEpXhUU/' target='_blank' rel='noopener noreferrer'>CSEC 2020 Last Chance Applications</a> Form
+                <h4>Director of Internal Affairs</h4>
+                <h4>Events Director</h4>
+                <h4>Lecturer/Guest lecturer</h4>
+            </div>
+            <h2 className='about__title'>2020 General Election Results</h2>
             <div>
                 <h4>President (Requires 1 year as a CSEC Exec)</h4>
                 <p>
                     The President is the official spokesperson of the organization and provides direction for all components of the organization in
                     a manner consistent with the organization’s constitution and policies.<br></br>
-                    Current: <b>Kevin S.</b>
+                    Current: Kevin S. <br></br>
+                    Elected: <b>Tabeeb Y.</b>
                 </p>
                 <h4>Director of Internal Affairs</h4>
                 <p>
-                    The Director of Internal Affairs act as the public relations of the club. Their task is to direct events, handle the creation and distribution of promotional club material, and listen to the memberbase for suggestions and improvements towards the club.<br></br>
-                    Current: <b>Tabeeb Y.</b>
+                    The Director of Internal Affairs act as the public relations of the club within the student community. Their task is to direct events, handle the creation and distribution of promotional club material, and listen to the memberbase for suggestions and improvements towards the club.<br></br>
+                    Current: Tabeeb Y. <br></br>
+                    Elected: <b>None</b>
+                </p>
+                <h4>Director of External Affairs</h4>
+                <p>
+                    The Director of External Affairs act as the public relations of the club with our external sponsors. Their task is to handle potential sponsors <br></br>
+                    Current: Tabeeb Y. <br></br>
+                    Elected: <b>Alexander C.</b>
                 </p>
                 <h4>Events Director</h4>
                 <p>
-                    Current: None
+                    Current: None <br></br>
+                    Elected: <b>None</b>
                 </p>
                 <h4>Marketing Director</h4>
                 <p>
                     The Marketing Director creates meaningful visuals and come up with creative ways to advertise the CSEC brand across multiple social media channels<br></br>
-                    Current: <b>Charmaine Y. and Paolo V.</b>
+                    Current: Charmaine Y. and Paolo V. <br></br>
+                    Elected: <b>Charmaine Y.</b>
                 </p>
                 <h4>Director of Software Development / System Administration</h4>
                 <p>
                     The Director of Software Development acts as a liaison amongst members in the organization. They will primarily deal with website administration and other technical aspects. It is also the responsibility of the director to record a summary of each lecture
                 session and general meeting and post it on the relevant media for access to all general members. They will be responsible for the website and other technical projects in the working (CMSClubs ... etc). <br></br>
-                Current: <b>Samiul H. and Frederic P.</b>
+                Current: Samiul H. and Frederic P. <br></br>
+                Elected: <b>Keshavaa S.</b>
                 </p>
-
             </div>
-            <h2 className='about__title'>2020-2021 Positions</h2>
+            <h2 className='about__title'>Upcoming 2020-2021 Positions</h2>
             <div>
                 <p>
                     These positions will be opening in the following 2020-2021 School Year. Applications will start around Aug - Sept 2020.
@@ -104,7 +107,6 @@ const PositionsPage = () => {
                 <h4>Game Division Leader (Requires one year as a Game Divison Exec)</h4>
                 <h4>Game Division Executive</h4>
                 <h4>Social Media Gurus</h4>
-                <h4>Guest Lecturer and Lecturers</h4>
             </div>
         </Container>
     );
